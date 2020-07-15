46cbd9e63fce7815729690297732de45bf50180e
michael.mansour256@yahoo.com <michael.mansour256@yahoo.com>
2020-07-15 19:54:15 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("I Am Poor"),
+        backgroundColor: Colors.blue[700],
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+          child: Image(
+        image: AssetImage('images/rock-icon-8527.png'),
+      )),
+    ),
+  ));
+}
